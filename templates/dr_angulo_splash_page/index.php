ab926a0c4b6c8dfee7de928ce20022e7ca74484e
TheJadeEmperor <benjamin_louie@drugfree.org>
2018-09-28 19:59:58 -0400

    delete old templates & added new templates

---
+++ templates/dr_angulo_splash_page/index.php
@@ -0,0 +1,64 @@
+<?php 
+$javascript = 'onclick="this.value=\'\'"';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Translator Jobs</title>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <link rel="shortcut icon" href="css/images/favicon.ico" />
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <script src="js/jquery-1.7.2.min.js" type="text/javascript"></script>
+    <script src="js/html5.js" type="text/javascript"></script>
+    <script src="js/functions.js" type="text/javascript"></script>
+</head>
+<body>
+<section id="wrapper">
+    <section id="bg">
+        <img src="css/images/bg.jpg" alt="" />
+    </section>
+    <!-- /#bg -->
+
+    <section class="content-box">
+        <!--<img src='http://i.realtranslatorjobs.com/home-urgent.png' />-->
+
+
+        <section class="blackbox">
+            
+            <h2>What <span class="red">Health Obstacles</span> are getting in your way? <span class="red">Conquer</span> your health obstacles!</h2>
+			<p>Are You Suffering from Body Pains, Low Energy, Constantly Sick, or Suffering from a Disease?</p>			
+			<p>Enter your e-mail for a <span class="red">FREE REPORT</span> for <br />5 strategies to optimize your health</p>
+
+			<form action="http://backtogetheragain.us8.list-manage1.com/subscribe/post?u=d388886f0417936d67cf31146&amp;id=110f91bac9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+			<div class="mc-field-group">
+				<input <?php echo $javascript?> type="email" name="EMAIL" class="required email field" id="mce-EMAIL" value="Enter your best email">
+			</div>
+			<div class="mc-field-group">
+				<input <?php echo $javascript?> type="text" name="FNAME" class="field" id="mce-FNAME" value="Enter your first name">
+			</div>
+			<div id="mce-responses" class="clear">
+			<div class="response" id="mce-error-response" style="display:none"></div>
+			<div class="response" id="mce-success-response" style="display:none"></div>
+			<p></p></div>
+			<p>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--></p>
+			<div style="position: absolute; left: -5000px;"><input type="text" name="b_d388886f0417936d67cf31146_110f91bac9" value=""></div>
+			<div class="clear"><input type="submit" value="Get Instant Access" name="subscribe" id="mc-embedded-subscribe" class="submit button"></div>
+			</form>
+            
+           
+            <div class="note">Your information will *never* be shared <br />or sold to a 3rd party.</div>
+			
+			<br />
+			<p>What would you do with more energy & a healthier body?</p>
+<p>Perhaps hike or train with your partner? Play more actively with your kids and/or grand kids? Play a sport after work? Or simply move more and stay active enough to keep lifestyle diseases at bay?</p>
+        </section>
+        <!-- /.blackbox -->
+        <footer class="footer">
+            Created By <a href="http://www.manhattanbackpain.com">Dr. Angulo, DC</a>
+        </footer>
+    </section>
+    <!-- /.content-box -->
+</section>
+</body>
+</html>
