712823a23aa18133ef5489becd0f2fb1ae5a5245
TheJadeEmperor <TheJadeEmperor@users.noreply.github.com>
2024-09-14 11:36:51 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php
 
 $adminEmail = 'kaiba.corporation.llc@gmail.com';
-$adminEmail = '4158897015@tmomail.net';
+// $adminEmail = '4158897015@tmomail.net';
 
 if($_POST['message']) {
 	$error = preg_match('/\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i', $_POST['email']) ? '' : 'INVALID EMAIL ADDRESS';
